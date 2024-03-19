352ed6f76e66830c4e0a70e08151acd483641b53
KaushalCorp <kaushalvarunkv@gmail.com>
2024-03-19 20:20:38 +0530

    Added Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:swiggy/pages/intro_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     name: 'swiggy-varun',
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
